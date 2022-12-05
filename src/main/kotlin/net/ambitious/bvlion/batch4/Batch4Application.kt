3e4425afa59e058aff8d29db7628e5903d18e177
bvlion <bvltiggeari@ambitious-i.net>
2022-12-05 18:29:57 +0900

    initialize

---
+++ src/main/kotlin/net/ambitious/bvlion/batch4/Batch4Application.kt
@@ -0,0 +1,11 @@
+package net.ambitious.bvlion.batch4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Batch4Application
+
+fun main(args: Array<String>) {
+	runApplication<Batch4Application>(*args)
+}
---
+++ src/test/kotlin/net/ambitious/bvlion/batch4/Batch4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.ambitious.bvlion.batch4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Batch4ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
